b9fff7f5bc0fd55c7204f7ac0c105e075b549923
Aliaksandr Baranouski <egefix@gmail.com>
2024-03-02 11:25:29 +0100

    tread

--- main.cpp
+++ main.cpp
@@ -7,7 +7,17 @@
 
 int main(int argc, char *argv[])
 {
+    int threads = 0;
+    for(int i{1}; i < argc; ++i)
+    {
+        try {
+            threads = std::stoi(argv[i]);
+            std::cout << threads;
 
+        } catch (const std::invalid_argument& e) {
+        }
+        break;
+    }
     try {
 
         ConfigJSON x{};
@@ -29,7 +39,7 @@ int main(int argc, char *argv[])
                 config = x.get_list(true);
             if(requests == nullptr)
                 requests = y.get_list(true);
-            SearchServer search{config, requests};
+            SearchServer search{config, requests, threads};
             Answers answer{search.get_RelativeIndex()};
             if(answer.get_answers(x.get_max_responses()))
             {
